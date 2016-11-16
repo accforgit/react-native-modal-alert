8cd82c35625570355a78d852a07b4da97ac9c4f4
zhuhui <2990956923@qq.com>
2016-11-16 12:32:14 +0800

    'init'

--- README.md
+++ README.md
@@ -1,2 +1,125 @@
 # react-native-modal-alert
-仿AlertIOS的弹窗组件，pure javascript，兼容IOS &amp; Android
+> transform from (rn-animate-alert)[https://github.com/parkerdan/rn-animate-alert]
+
+---
+RN官方提供的兼容Android与IOS的Alert组件，样式定制化程度太低，难以满足大部分的需求
+这里提供的一个仿照AlertIOS的组件，基于(rn-animate-alert)[https://github.com/parkerdan/rn-animate-alert]进行了一番改造，
+最终的样式近乎于AlertIOS。
+
+感谢(parkerdan)[https://github.com/parkerdan]提供的(rn-animate-alert)[https://github.com/parkerdan/rn-animate-alert]组件
+
+---
+
+
+
+`npm install --save https://github.com/accforgit/react-native-modal-alert.git`
+
+- Will not animate unless you specify what direction.
+
+`springFromTop={true}` or `springFromBottom={true}`
+
+#### Just copy the example and you'll see
+
+```js
+import React, { Component } from 'react';
+import { View, TouchableOpacity, AppRegistry, Text } from 'react-native';
+import Alert from 'rn-animate-alert';
+
+export default class Example extends Component {
+
+  constructor(){
+    super();
+    this.state={
+      renderAlert: false
+    }
+  };
+
+
+
+  render(){
+    return(
+      <View style={{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:'deeppink'
+      }}>
+        <Text style={{color:'white',fontSize:30, textAlign:'center'}}>I'm a React-Native Component</Text>
+        <TouchableOpacity
+          onPress={
+            () => this.setState({renderAlert:true})
+          }
+          style={{
+            borderRadius:5,
+            padding:20,
+            alignItems:'center',
+            backgroundColor:'aqua'
+          }}>
+          <Text>Press Me!</Text>
+        </TouchableOpacity>
+        {this.renderAlert()}
+      </View>
+    )
+  };
+
+  renderAlert(){
+    if (this.state.renderAlert) {
+      return(
+        <Alert
+          title={'I\'m an alert and I animated in!!'}
+          detailText={'Watch me animate out too!!'}
+
+          springFromBottom={true}
+
+          onConfirm={
+            () => {
+              console.log('do stuff');
+              this.setState({renderAlert:false})
+            }
+          }
+          onCancel={
+            () => {
+              console.log('Do other stuff');
+              this.setState({renderAlert:false})
+            }
+          }/>
+      )
+    }
+  };
+
+};
+
+AppRegistry.registerComponent('Example', () => Example);
+```
+
+### There's a shit ton of props available to style this thing HOWEVER YOU WANT
+
+##### Available props
+
+```js
+onConfirm: React.PropTypes.func.isRequired,
+onCancel: React.PropTypes.func.isRequired,
+
+title: React.PropTypes.string.isRequired,
+titleTextStyle: React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+
+detailText: React.PropTypes.string,
+detailTextStyle: React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+
+confirmText: React.PropTypes.string,
+confirmTextStyle:React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+confirmButtonStyle:React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+
+cancelText: React.PropTypes.string,
+cancelTextStyle: React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+cancelButtonStyle:React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+
+backgroundColor: React.PropTypes.string,
+
+containerStyle: React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+
+springFromBottom: React.PropTypes.bool,
+springFromTop: React.PropTypes.bool,
+
+zIndex: React.PropTypes.number,
+```
---
+++ index.js
@@ -0,0 +1,210 @@
+'use strict'
+
+import React, { Component, PropTypes } from 'react';
+import {
+View,
+Dimensions,
+Text,
+TouchableOpacity,
+Animated,
+Easing
+} from 'react-native';
+
+const { height,width } = Dimensions.get('window');
+
+const propTypes = {
+onConfirm: React.PropTypes.func.isRequired,
+onCancel: React.PropTypes.func.isRequired,
+
+title: React.PropTypes.string.isRequired,
+titleTextStyle: React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+
+detailText: React.PropTypes.string,
+detailTextStyle: React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+
+confirmText: React.PropTypes.string,
+confirmTextStyle:React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+confirmButtonStyle:React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+
+cancelText: React.PropTypes.string,
+cancelTextStyle: React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+cancelButtonStyle:React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+
+backgroundColor: React.PropTypes.string,
+
+containerStyle: React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+
+springFromBottom: React.PropTypes.bool,
+springFromTop: React.PropTypes.bool,
+
+zIndex: React.PropTypes.number, // try and avoid this.  only needed if you used zIndex elsewhere
+
+};
+
+export default class Alert extends Component {
+
+ constructor(props){
+   super(props);
+   this.dismiss = this.dismiss.bind(this);
+
+   this.state = {
+   };
+ };
+
+ componentWillMount(){
+   let xOffset = new Animated.Value(0);
+   let yOffset = new Animated.Value(0);
+
+
+   if (this.props.springFromBottom) {
+     yOffset.setValue(1)
+   }
+   if (this.props.springFromTop) {
+     yOffset.setValue(-1)
+   }
+
+   this.setState({xOffset:xOffset,yOffset:yOffset})
+ };
+
+ componentDidMount(){
+   Animated.parallel([
+     Animated.spring(this.state.yOffset,{
+       toValue:0
+     })
+   ]).start()
+ };
+
+ dismiss(cb){
+   let toValue = 0;
+   if (this.props.springFromBottom) {
+     toValue = 1;
+   }
+   if (this.props.springFromTop) {
+     toValue = -1;
+   }
+   Animated.timing(this.state.yOffset,{
+     toValue: toValue,
+     duration: 200,
+     easing: Easing.linear
+   }).start(cb)
+ };
+
+ renderText(){
+   if (this.props.detailText) {
+     return(
+       <Text style={[{
+         marginBottom:10
+       },this.props.detailTextStyle]}>
+         {this.props.detailText}
+       </Text>
+     )
+   }
+ };
+
+ render(){
+   return(
+     <View style={{
+       zIndex:this.props.zIndex || 0,
+       position:'absolute',
+       top:0,
+       left:0,
+       height:height,
+       width:width,
+       backgroundColor: this.props.backgroundColor || 'rgba(111,111,111,.9)',
+       alignItems:'center',
+       justifyContent:'center'
+     }}>
+       <Animated.View style={[{
+         backgroundColor: 'white',
+         borderRadius:8,
+         width:width*0.8,
+         height:width*0.4,
+         flexDirection:'column',
+         paddingTop:12
+       },this.props.containerStyle,{
+         transform:[{
+           translateY:this.state.yOffset.interpolate({
+             inputRange: [-1,0,1],
+             outputRange: [-500,0,500]
+           })}]}]}>
+
+         <Text style={[{
+           fontSize:20,
+           fontWeight:'500',
+           marginBottom:10,
+           textAlign:'center',
+           color:'#666'
+         },this.props.titleTextStyle]}>
+           {this.props.title}
+         </Text>
+         <View style={{
+           position:'absolute',
+           bottom:0,
+           width:width*0.8
+         }}>
+          <View style={{borderBottomWidth:1,borderColor:"#E8E8EA",height:width*0.16,flexDirection:'row',alignItems:'center',justifyContent:'center'}}>
+            <Text>{this.renderText()}</Text>
+          </View>
+          <View style={{
+            flexDirection: 'row'
+            }}>
+            <View style={{
+              flex:1
+              }}>
+              <TouchableOpacity
+                style={[{
+                  padding:10,
+                  backgroundColor:'#fff',
+                  alignItems:'center',
+                  marginLeft:4,
+                  marginBottom:4,
+                  borderRightWidth:1,
+                  borderColor:'#E8E8EA'
+                },this.props.cancelButtonStyle]}
+                onPress={
+                  () => this.dismiss(this.props.onCancel)
+                }>
+                <Text style={[{
+                  color:'#666',
+                  fontWeight:'700'
+                },this.props.cancelTextStyle]}>
+                  {(this.props.cancelText) ? this.props.cancelText:'Cancel'}
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={{
+              flex:1,
+              paddingLeft:10
+            }}>
+              <TouchableOpacity
+                style={[{
+                  padding:10,
+                  backgroundColor:'#fff',
+                  alignItems:'center',
+                  marginRight:4,
+                  marginBottom:4,
+                  borderColor:'#E8E8EA'
+                },this.props.confirmButtonStyle]}
+                onPress={
+                  () => this.dismiss(this.props.onConfirm)
+                }>
+                <Text style={[{
+                  color:'#666',
+                  fontWeight:'700',
+                },this.props.confirmTextStyle]}>
+                  {(this.props.confirmText) ? this.props.confirmText:'Ok'}
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+         </View>
+       </Animated.View>
+     </View>
+   )
+ };
+
+};
+
+Alert.propTypes = propTypes;
